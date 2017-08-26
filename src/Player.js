99833f344d5a00ab6e07a99a1bb3686168134f38
ritorrehuelamo <torrehuelamo@gmail.com>
2017-08-26 18:56:24 +0200

    Adding Player's test and Player Object

---
+++ src/Player.js
@@ -0,0 +1,24 @@
+function Player(health, power, defense, x, y, range) {
+  this.health = health
+  this.power = power
+  this.defense = defense
+  this.x = x
+  this.y = y
+  this.range = range
+}
+
+Player.prototype.moveUp = function () {
+
+}
+
+Player.prototype.blendDown = function () {
+  
+}
+
+Player.prototype.moveLeft = function () {
+
+}
+
+Player.prototype.moveRight = function () {
+
+}
