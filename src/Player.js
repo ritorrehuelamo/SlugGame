b99c890eb6c12032688eaa3a364cf4d1709ae56c
ritorrehuelamo <torrehuelamo@gmail.com>
2017-08-28 00:53:09 +0200

    Player movement & srcoll ~

--- src/Player.js
+++ src/Player.js
@@ -1,24 +1,34 @@
-function Player(health, power, defense, x, y, range) {
-  this.health = health
-  this.power = power
-  this.defense = defense
-  this.x = x
-  this.y = y
-  this.range = range
+function Player (x, y) {
+	this.x = x
+	this.y = y
+	this.score = 0
+	this.lifes = 3
+	this.player = $('<div>').attr('id', 'player')
+	this.player.css({top: this.x, left: this.y, position: 'absolute'})
+	$('#board').append(this.player)
 }
 
-Player.prototype.moveUp = function () {
-
-}
-
-Player.prototype.blendDown = function () {
-  
+Player.prototype.restart = function (x, y, score, lifes) {
+	this.x = x
+	this.y = y
+	this.score = score
+	this.lifes = lifes
 }
 
 Player.prototype.moveLeft = function () {
-
+	$('#board').animate({left: this.x - 100}, 1500 )
+	this.x = parseInt($('#player').css('left'))
+	this.x += 10
+	$('#player').css('left', this.x + 'px')
 }
 
 Player.prototype.moveRight = function () {
+	$('#board').animate({left: this.x + 100}, 1500 )
+	this.x = parseInt($('#player').css('left'))
+	this.x -= 10
+	$('#player').css('left', this.x + 'px')
+}
+
+Player.prototype.jump = function () {
 
 }
